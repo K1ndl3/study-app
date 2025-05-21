7d0ce75c747708c39b9405d6635f28eef3415e01
K1ndl3 <HUYBUI02@csu.fullerton.edu>
2025-05-20 23:32:17 -0700

    minor fix

--- src/Todo/Todo.jsx
+++ src/Todo/Todo.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import "./Todo.css"
 function Todo() {
 
-    const DETELE_BUTN_SVG = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+    const DETELE_BTN_SVG = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                             </svg>;
 
@@ -23,7 +23,7 @@ function Todo() {
                          </svg>;
 
 
-    const ADD_BUTN_SVG = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
+    const ADD_BTN_SVG = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
                          <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14Zm.75-10.25v2.5h2.5a.75.75 0 0 1 0 1.5h-2.5v2.5a.75.75 0 0 1-1.5 0v-2.5h-2.5a.75.75 0 0 1 0-1.5h2.5v-2.5a.75.75 0 0 1 1.5 0Z" clip-rule="evenodd" />
                          </svg>;
 
@@ -94,7 +94,7 @@ function Todo() {
                     onChange={(e) => handleInput(e)}
                 ></input>
                 <span className="utilitites">
-                    <button className="enter-button" onClick={() => addTask()}>{ADD_BUTN_SVG}</button>
+                    <button className="enter-button" onClick={() => addTask()}>{ADD_BTN_SVG}</button>
                     <p className="progress-count">{numTaskFinished} / {taskLeft}</p>
                 </span>
             </div>
@@ -109,7 +109,7 @@ function Todo() {
                         <span className="task">{task}</span>
                         <span className="edit-button">
                             <button className="finish-button" onClick={() => finishTask(index)}>{FINISH_BTN_SVG}</button>
-                            <button className="delete-button" onClick={() => deleteTask(index)}>{DETELE_BUTN_SVG}</button>
+                            <button className="delete-button" onClick={() => deleteTask(index)}>{DETELE_BTN_SVG}</button>
                         </span>
                     </li>
                     )}
